14368a4ca98b2cb49db58b4cbd6f1e204e85b4aa
Wybren-Jongstra <wybren.jongstra@student.stenden.com>
2015-03-25 11:19:30 +0100

    Smartwatch with time:)

---
+++ Smartwatch/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Smartwatch
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DateTime currentTime;
+        Boolean runningApp;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            runningApp = true;
+            Thread timeNow = new Thread(new ThreadStart(getCurrentTime));
+            timeNow.Start();
+            
+            //Console.WriteLine(Time._instance.getTime());
+        }
+
+        public void getCurrentTime()
+        {
+            while (runningApp == true)
+            {
+                currentTime = Time._instance.getTime();
+                Console.WriteLine(currentTime.ToString());
+                Thread.Sleep(3000);
+            }
+            
+        }
+    }
+}
---
+++ Smartwatch/Time.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smartwatch
+{
+    class Time
+    {
+        public static readonly Time _instance = new Time();
+
+        public DateTime getTime()
+        {
+            return DateTime.Now;
+        }
+    }
+}
