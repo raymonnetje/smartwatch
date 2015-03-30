1eecf366958ec03af70ec40b5a0558bc90e10472
Wybren-Jongstra <wybren.jongstra@student.stenden.com>
2015-03-30 13:08:21 +0200

    working am/pm

--- Smartwatch/MainWindow.xaml.cs
+++ Smartwatch/MainWindow.xaml.cs
@@ -30,6 +30,7 @@ namespace Smartwatch
         {
             InitializeComponent();
             runningApp = true;
+            LabelState.Dispatcher.Invoke(() => LabelState.Opacity = 0);
             changeState = new TimeStateClient(new TwentyFourHoursFormat());
             Thread timeNow = new Thread(new ThreadStart(getCurrentTime));
             timeNow.Start();
@@ -41,7 +42,6 @@ namespace Smartwatch
             {
                 currentTime = Time._instance.getTime();
                 writeTime(currentTime, timeformat);
-                //labelTime.Dispatcher.Invoke(() => labelTime.Content = currentTime.ToString());
                 Thread.Sleep(2000);
             }
             
@@ -52,6 +52,15 @@ namespace Smartwatch
             try
             {
                 labelTime.Dispatcher.Invoke(() => labelTime.Content = currentTime.ToString(timeformat));
+                Label_Datum.Dispatcher.Invoke(() => Label_Datum.Content = currentTime.Day.ToString() + " " + getMonthName(currentTime.Month.ToString()));
+                if ((timeformat == "hh:mm") && (currentTime.Hour >= 12))
+                {
+                    LabelState.Dispatcher.Invoke(() => LabelState.Content = "PM");
+                }
+                else if ((timeformat == "hh:mm") && (currentTime.Hour < 12))
+                {
+                    LabelState.Dispatcher.Invoke(() => LabelState.Content = "AM");
+                }
             }
             catch (TaskCanceledException exception)
             {
@@ -61,16 +70,36 @@ namespace Smartwatch
             }
         }
 
+        public string getMonthName(string month)
+        {
+            switch (month)
+            {
+                case "1": return "Januari";
+                case "2": return "Februari";
+                case "3": return "Maart";
+                case "4": return "April";
+                case "5": return "Mei";
+                case "6": return "Juni";
+                case "7": return "Juli";
+                case "8": return "Augustus";
+                case "9": return "September";
+                case "10": return "Oktober";
+                case "11": return "November";
+                case "12": return "December";
+                default: return "N.B.";
+            }
+        }
+
         public void changeTimeFormat(string twelveOrtwentyfour)
         {
             if (twelveOrtwentyfour == "Smartwatch.TwelveHoursFormat")
             {
-                LabelState.Content = "TwelveHoursFormat";
+                LabelState.Dispatcher.Invoke(() => LabelState.Opacity = 100);
                 timeformat = "hh:mm";
             }
             else
             {
-                LabelState.Content = "TwentyFourHoursFormat";
+                LabelState.Dispatcher.Invoke(() => LabelState.Opacity = 0);
                 timeformat = "HH:mm";
             }
         }
